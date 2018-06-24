e80e6b206514bb7bb3f5ec980b2bdf4569128747
zwx385745 <517040960@qq.com>
2018-06-24 16:11:15 +0800

    q

---
+++ paascloud-discovery/src/main/java/com/paascloud/discovery/PaasCloudDiscoveryApplication.java
@@ -0,0 +1,27 @@
+package com.paascloud.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * The class Paas cloud discovery application.
+ *
+ * @author paascloud.net@gmail.com
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class PaasCloudDiscoveryApplication {
+
+	/**
+	 * The entry point of application.
+	 *
+	 * @param args the input arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(PaasCloudDiscoveryApplication.class, args);
+	}
+}
